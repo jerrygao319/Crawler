bf1b02315ffdb3f76d3b4400c58bd40a0aa48dc2
Jerry <gaozhiwei1993@gmail.com>
2019-10-23 17:08:00 +0900

    socket

--- twitch_socket.py
+++ twitch_socket.py
@@ -6,7 +6,7 @@ config = {
     "port": 6667,
     "nickname": "renaissancevector",
     "token": "oauth:j4c2ruphbc7v5v3eyhlt12lp1blipy",
-    "channel": "#pvpx"
+    "channel": "#myth"
 }
 
 sock = socket.socket()
@@ -17,17 +17,15 @@ sock.send(f'NICK {config["nickname"]}\r\n'.encode("utf-8"))
 sock.send(f'JOIN {config["channel"]}\r\n'.encode("utf-8"))
 
 try:
-    i = 0
-    while i < 99:
+    while True:
         resp = sock.recv(2048).decode("utf-8")
         if resp.startswith("PING"):
             print("PING:", resp)
         else:
-            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            with open("twitch_chat_log", 'a') as f:
-                f.write(str(now))
+            print("Received:", resp)
+            with open("twitch_chat_log_191023.log", 'a') as f:
+                f.write(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
                 f.write(str(resp))
-        i += 1
 except Exception:
     print("Error")
 finally:
