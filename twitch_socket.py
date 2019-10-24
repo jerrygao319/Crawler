2cbbed071e12c6072971e46683c821bb1535f4cf
Jerry <gaozhiwei1993@gmail.com>
2019-10-24 12:43:05 +0900

    socket

--- twitch_socket.py
+++ twitch_socket.py
@@ -7,7 +7,7 @@ config = {
     "port": 6667,
     "nickname": "renaissancevector",
     "token": "oauth:j4c2ruphbc7v5v3eyhlt12lp1blipy",
-    "channel": "#starladder_cs_en"
+    "channel": "#nickmercs"
 }
 
 sock = socket.socket()
@@ -25,30 +25,30 @@ try:
         if not resp:
             break
         if resp.startswith("PING"):
-            print("PING:", resp)
+            print("PING: ", resp)
+        elif resp.startswith(":tmi.twitch.tv"):
+            print("HEADER: ", resp)
         else:
-            print("Received:", resp)
-            if not resp.startswith(":tmi.twitch.tv"):
-                respList = resp.splitlines()
-                for respSingle in respList:
-                    if respSingle.startswith(":" + config["nickname"]):
-                        continue
-                    infoList = respSingle.split(config["channel"])
-                    usernameInfoList = infoList[0].split(":")
-                    if len(usernameInfoList) >= 2:
-                        username_search = re.search(r"(.*)!.*@(.*) ", usernameInfoList[1], re.IGNORECASE)
-                        if username_search:
-                            username = username_search.group(1)
-                            userURL = username_search.group(2)
-                    if len(infoList) >= 2:
-                        comment = infoList[1].split(":")[1]
-                    with open("twitch_chat_log_191023_2.log", 'a+') as f:
-                        f.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "\t")
-                        f.write(username, "\t", comment, "\n")
+            print("Received: ", resp)
+            respList = resp.splitlines()
+            for respSingle in respList:
+                if respSingle.startswith(":" + config["nickname"]):
+                    continue
+                infoList = respSingle.split(config["channel"])
+                usernameInfoList = infoList[0].split(":")
+                if len(usernameInfoList) >= 2:
+                    username_search = re.search(r"(.*)!.*@(.*) ", usernameInfoList[1], re.IGNORECASE)
+                    if username_search:
+                        username = username_search.group(1)
+                        userURL = username_search.group(2)
+                if len(infoList) >= 2:
+                    comment = infoList[1].split(":")[1]
+                with open("twitch_chat_log_191024.log", 'a+') as f:
+                    f.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "\t")
+                    f.write(username, "\t", comment, "\n")
 
 except Exception as e:
     print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), " Error:", e)
 finally:
     f.close()
     sock.close()
-
