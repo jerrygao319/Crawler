06f25ff64b71673ca56c3e4092d11a426f1bf276
Jerry <gaozhiwei1993@gmail.com>
2019-12-23 16:45:27 +0900

    initial

--- vaccine_collection.sh
+++ vaccine_collection.sh
@@ -2,13 +2,13 @@
 source /etc/profile
 {
   echo  "$(date)" '-----Collect English ver. start....-----'
-  /usr/bin/python3 /home/gao/project/Crawler/index.py --lang en
+  python /home/gao/project/Crawler/index.py --lang en
   echo "$(date)" '-----Collect English ver. finished!-----'
 } >> /home/gao/project/Crawler/log/bash.log
 sleep 3
 {
   echo "$(date)" '-----Collect Japanese ver. start....-----'
-  /usr/bin/python3 /home/gao/project/Crawler/index.py --lang ja
+  python /home/gao/project/Crawler/index.py --lang ja
   echo -e "$(date)" '-----Collect Japanese ver. finished!-----\n'
 } >> /home/gao/project/Crawler/log/bash.log
 exit 0
